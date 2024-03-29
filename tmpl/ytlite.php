8354e8e504983e00f357f56f5050038830409cca
Plamen <pl71@users.noreply.github.com>
2024-03-29 13:29:23 +0200

    Update ytlite.php

--- tmpl/ytlite.php
+++ tmpl/ytlite.php
@@ -1,34 +1,72 @@
 <?php
-/**
- * Custom Fields - Video custom field
- *
- * @author brian@teeman.net
- * @copyright Copyright (c) 2022
- * @license GNU Public License v2
- * @link https://brian.teeman.net
+/* @package Joomla
+ * @copyright Copyright (C) Open Source Matters. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
  */
+defined( '_JEXEC' ) or die( 'Restricted access' );
 
-defined('_JEXEC') or die;
+jimport( 'joomla.plugin.plugin' );
 
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
-use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\Layout\FileLayout;
+use Joomla\CMS\Router\Route;
 
-if (empty($field->value))
+
+class plgContentYTL extends JPlugin
 {
-    return;
+	public function __construct(& $subject, $config) {
+		parent::__construct($subject, $config);
+		$this->loadLanguage();
+	}
+
+	public function onContentPrepare($context, &$article, &$params, $page = 0) {
+
+		// Don't run this plugin when the content is being indexed
+		if ($context == 'com_finder.indexer') {
+			return true;
+		}
+
+		$app 	= Factory::getApplication();
+		$view	= $app->input->get('view');
+		if ($view == 'tag') { return; }
+
+		$document		= Factory::getDocument();
+		
+		if ($document->getType() !== 'html' || !$app->isClient('site')) {
+            return;
+        }
+		
+		$lang 			= Factory::getLanguage();
+
+		// Start Plugin
+		$regex_ytl	='/{yt\/\s*(.*youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})\/?([^\/]*)\/?}/siU';
+		$groups 	= array();
+		$match		= preg_match($regex_ytl, $article->text, $groups);
+
+		// Start if count_matches
+		if ($match === 1) {
+
+			/** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
+			$wa = $document->getWebAssetManager();
+			$wa->registerAndUseStyle('plg_content_ytl', 'plg_content_ytl/lite-yt-embed.css');
+			$wa->registerAndUseScript('plg_content_ytl', 'plg_content_ytl/lite-yt-embed.js', [], ['defer' => true]);
+			
+			while ($match === 1) { 
+				 
+				$video	= $groups[2];
+				$url 	= $groups[1] . $video;
+				$title	= $groups[3];
+				$repl 	= '<lite-youtube videoid="' . $video . '" style="background-image: url(\'https://i.ytimg.com/vi/' . $video . '/maxresdefault.jpg\')"><a href="' . $url . '" class="lyt-playbtn" title="Play Video"><span class="lyt-visually-hidden">Play Video: ' . $title . '</span></a></lite-youtube>';
+
+				$article->text 	= preg_replace($regex_ytl, $repl, $article->text, 1); 
+				$match			= preg_match($regex_ytl, $article->text, $groups);
+			}
+			
+		}// end if match
+		return true;
+	}
 }
-
-/** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
-$wa = $this->app->getDocument()->getWebAssetManager();
-$wa->registerAndUseStyle('plg_fields_ytlite', 'plg_fields_ytlite/lite-yt-embed.css');
-$wa->registerAndUseScript('plg_fields_ytlite', 'plg_fields_ytlite/lite-yt-embed.js', [], ['defer' => true]);
-
-$url = $field->value;
-$regex = '/(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/m';
-preg_match($regex, $url, $matches);
-$videoid= $matches[1] ?? '';
 ?>
-<lite-youtube videoid="<?php echo $videoid; ?>" style="background-image: url('https://i.ytimg.com/vi/<?php echo $videoid; ?>/maxresdefault.jpg');">
-    <a href="<?php echo $url; ?>" class="lyt-playbtn" title="<?php echo Text::_('PLG_FIELDS_YTLITE_PLAY'); ?>"></a>
-</lite-youtube>
-
